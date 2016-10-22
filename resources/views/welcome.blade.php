03b5ac3d2a3fa431b48337cd19718d434070aa45
lj <2233460286@qq.com>
2016-10-22 16:40:19 +0800

    welcome 修改

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
                     <div class="panel-heading">Welcome</div>
 
                     <div class="panel-body">
-                        Your Application's Landing Page.
+                        国家级科研基地调查情况
                     </div>
                 </div>
             </div>
